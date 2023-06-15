f0ae3173d4be2ea8ce1c6b1b05a5a5e4c0501674
Maxim <maksim.pudeev@bk.ru>
2023-06-15 15:35:40 +0300

    feat: add 07-conditions-n-loops-tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -128,8 +128,10 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const first = rect1.top + rect1.height > rect2.top;
+  const second = rect1.left + rect1.width > rect2.left;
+  return first && second;
 }
 
 /**
@@ -217,9 +219,12 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
+
+// const a = 'The quick brown fox jumps over the lazy dog';
+// console.log(a.split('').reverse().join(''));
 
 /**
  * Reverse the specified integer number (put all digits in reverse order)
@@ -233,9 +238,12 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return num.toString().split('').reverse().join('');
 }
+
+// const a = 12345;
+// console.log(a.toString().split('').reverse().join(''));
 
 /**
  * Validates the CCN (credit card number) and return true if CCN is valid
@@ -275,6 +283,7 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
+
 function getDigitalRoot(/* num */) {
   throw new Error('Not implemented');
 }
@@ -324,8 +333,8 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  return num.toString(n);
 }
 
 /**
